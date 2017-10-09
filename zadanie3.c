04ae197f211cba092fe54997838a222dd376cfd9
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2017-10-09 21:15:04 +0200

    Add files via upload

---
+++ zadanie3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    a=5;
+    b=5;
+    int wynik1, wynik2, wynik3, wynik4, wynik5;
+    wynik1=a+b;
+    wynik2=a-b;
+    wynik3=a*b;
+    wynik4=a/b;
+    wynik5=a%b;
+
+    return 0;
+}
