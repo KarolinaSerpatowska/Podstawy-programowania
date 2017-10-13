92826aadb51b669f7c817c5f01b8e3fb40eb2220
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2017-10-13 15:51:13 +0200

    Create 13.10zad1.c

---
+++ 13.10zad1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int max;
+
+int main()
+{
+    int a,b,c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    if (a>b)
+    {
+            if (a>c)
+                max=a;
+            else
+                max=c;
+    }
+            else
+            {
+                if (b>c)
+            max=b;
+                else max=c;
+            }
+    
+    printf("%d", max);
+    
+    return 0;
+}
